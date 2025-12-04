52538b260ebd41e6a54d17fcb3a7126496f3813a
Bacar Abdourahim <hugorischmann@air-de-hugo.home>
2025-12-04 14:30:54 +0100

    Ajout du dossier

---
+++ pages/Dashboard.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 23 14:36:57 2025
+
+@author: ryles
+"""
+
---
+++ pages/Interprétation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 23 14:36:25 2025
+
+@author: ryles
+"""
+
